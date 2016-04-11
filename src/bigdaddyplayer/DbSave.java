058b525edc7b0afdccce198057d81e6db89c6937
root <root@linux-601v.suse>
2016-04-11 22:42:28 +0530

    Detail and playlist created

---
+++ src/bigdaddyplayer/DbSave.java
@@ -0,0 +1,440 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bigdaddyplayer;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author root
+ */
+public class DbSave {
+        //JDBC driver name and database URL
+        static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+        static final String DB_URL = "jdbc:mysql://localhost/BigDaddy";
+        //Database credentials
+        static final String USER = "root";
+        static final String PASS = "root";
+        
+    public void set_detail(String location,String name,String artist,String album,int rating,String genre,String comments){      
+        Connection conn = null;
+        Statement stmt = null;
+        
+        
+        
+            try{
+            Class.forName("com.mysql.jdbc.Driver");
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            //STEP 4: Execute a query
+            //System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "UPDATE OfDetail SET SName='"+name+"',Artist='"+artist+"',Rating="+rating+",Genre='"+genre+"',Comment='"+comments+"' WHERE location='"+location+"'";
+            Boolean ret = stmt.execute(sql);
+            System.out.println("Return value is : " + ret.toString() );
+            /*
+            sql = "SELECT id, first, last, age FROM Employees";
+            ResultSet rs = stmt.executeQuery(sql);
+            //STEP 5: Extract data from result set
+            while(rs.next()){
+                    //Retrieve by column name
+                    int id
+                     = rs.getInt("id");
+                    int age = rs.getInt("age");
+                    String first = rs.getString("first");
+                    String last = rs.getString("last");
+                    //Display values
+                    System.out.print("ID: " + id);
+                    System.out.print(", Age: " + age);
+                    System.out.print(", First: " + first);
+                    System.out.println(", Last: " + last);
+            }*/
+            //STEP 6: Clean-up environment
+            //rs.close();
+            stmt.close();
+            conn.close();
+            }
+            catch(SQLException | ClassNotFoundException se)
+            {
+            }finally{
+            //finally block used to close resources
+            try{
+            if(stmt!=null) {
+                stmt.close();
+            }
+            }catch(SQLException se2){
+            }// nothing we can do
+            try{
+                if(conn!=null) {
+                    conn.close();
+                }
+            }catch(SQLException se){
+            }//end finally try
+            }//end try
+                System.out.println("Goodbye!");
+            }//end main
+            //end FirstExample
+    
+    
+    public void set_detail(String location){      
+            Connection conn = null;
+            Statement stmt = null;
+        
+        
+        
+            try{
+            Class.forName("com.mysql.jdbc.Driver");
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            //STEP 4: Execute a query
+            //System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql="Select SNo from OfDetail where location='"+location+"'";
+            ResultSet ret1 = stmt.executeQuery(sql);
+            
+             if (!ret1.next() ) {
+                 System.out.println("no data");
+                // do { // Use 'do...while' to process the first row, while continuing to process remaining rows
+
+                    // } while (resultSet.next());
+                    
+                    sql = "INSERT INTO OfDetail(location) Values('"+location+"')";
+                    boolean ret = stmt.execute(sql);
+                    System.out.println("Return value is : " + ret );
+            
+            stmt.close();
+            conn.close();
+                }
+             else{
+                 stmt.close();
+                conn.close();
+                return;
+             }
+
+            }
+            catch(SQLException se)
+            {
+            }catch(ClassNotFoundException e){
+            }finally{
+            //finally block used to close resources
+            try{
+            if(stmt!=null) {
+                stmt.close();
+            }
+            }catch(SQLException se2){
+            }// nothing we can do
+            try{
+                if(conn!=null) {
+                    conn.close();
+                }
+            }catch(SQLException se){
+            }//end finally try
+            }//end try
+            
+                System.out.println("Goodbye!");
+            }//end main
+            //end FirstExample
+    
+    public String set_Playlist(String loc,String pname){
+        Connection conn = null;
+        Statement stmt = null;
+        String name = null;
+        
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            //STEP 4: Execute a query
+            //System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            
+            sql="Select SName from OfDetail where location='"+loc+"'";
+            ResultSet ret1 = stmt.executeQuery(sql);
+            
+             /*if (!ret1.next() ) {
+                 //System.out.println("no data");
+                // do { // Use 'do...while' to process the first row, while continuing to process remaining rows
+
+                    // } while (resultSet.next());
+                    
+                    sql = "INSERT INTO OfPlaylist(location) Values('"+location+"')";
+                    boolean ret = stmt.execute(sql);
+                    System.out.println("Return value is : " + ret );
+            
+            stmt.close();
+            conn.close();
+                }*/
+             while(ret1.next()){
+                    //Retrieve by column name
+                    name = ret1.getString("SName");
+                   
+             }
+             sql = "Insert into OfPlaylist(SName,location,PlName) Values('"+name+"','"+loc+"','"+pname+"')";
+                    boolean ret = stmt.execute(sql);
+                    System.out.println("Return value is : " + ret );
+                
+                 
+            //stmt.close();
+            //conn.close();
+                
+             
+
+            }
+            catch(SQLException | ClassNotFoundException se)
+            {
+            }finally{
+            //finally block used to close resources
+            try{
+            if(stmt!=null) {
+                stmt.close();
+            }
+            }catch(SQLException se2){
+            }// nothing we can do
+            try{
+                if(conn!=null) {
+                    conn.close();
+                }
+            }catch(SQLException se){
+            }//end finally try
+            }//end try
+            
+                System.out.println("Goodbye!");
+                return name;
+            }//end main
+    
+    public void plsave(String pname){
+        
+        
+        Connection conn = null;
+        Statement stmt = null;
+        
+        
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            //STEP 4: Execute a query
+            //System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql="Select SNo from PlName where Pname='"+pname+"'";
+            ResultSet ret1 = stmt.executeQuery(sql);
+            if (!ret1.next() ) {
+                sql="INSERT Into PlName(Pname) Values('"+pname+"')";
+                boolean ret2 = stmt.execute(sql);
+            
+            
+            }
+            else{
+                System.out.println("Playlist already there");
+            }
+            
+            
+            
+            
+        }   catch (SQLException ex) {
+                Logger.getLogger(DbSave.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(DbSave.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    
+    try{
+            if(stmt!=null)
+                stmt.close();
+            }catch(SQLException se2){
+            }// nothing we can do
+            try{
+                if(conn!=null)
+                conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end finally try
+            }//end try
+            
+    
+    
+    public String[] get_pname(){
+        Connection conn = null;
+        Statement stmt = null;
+        String[] pln = new String[100];
+        
+        
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            //STEP 4: Execute a query
+            //System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "Select Pname from PlName";
+            ResultSet rs = stmt.executeQuery(sql);
+            int i = 0;
+             while(rs.next()){
+                    //Retrieve by column name
+                    pln[i] = rs.getString("Pname");
+                    i=i+1;
+                    
+            }
+            pln[i] = "eof";
+            rs.close();
+            System.out.println(pln[0]);
+        }   
+            catch (SQLException ex) {
+                Logger.getLogger(DbSave.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(DbSave.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            try{
+            if(stmt!=null) {
+                stmt.close();
+            }
+            }catch(SQLException se2){
+            }// nothing we can do
+            try{
+                if(conn!=null) {
+                    conn.close();
+                }
+            }catch(SQLException se){
+            }//end finally try
+            
+            return pln;
+            
+            }
+            
+            
+    
+    public String[] showDet(String location){
+        Connection conn = null;
+        Statement stmt = null;
+        String[] data = new String[7];
+        
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            //STEP 4: Execute a query
+            //System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "Select * from OfDetail where location='"+location+"'";
+            ResultSet ret1 = stmt.executeQuery(sql);
+            while(ret1.next()){
+                    //Retrieve by column name
+                    data[0] = ret1.getString("SNo");
+                    data[1] = ret1.getString("SName");
+                    data[2] = ret1.getString("Artist");
+                    data[3] = ret1.getString("Album");
+                    data[4] = ret1.getString("Rating");
+                    data[5] = ret1.getString("Genre");
+                    data[6] = ret1.getString("Comment");
+            }
+            
+                    
+                    
+                    
+                    
+                    
+         
+        }   catch (ClassNotFoundException ex) {
+                Logger.getLogger(DbSave.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SQLException ex) {
+                Logger.getLogger(DbSave.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        try{
+            if(stmt!=null) {
+                stmt.close();
+            }
+            }catch(SQLException se2){
+            }// nothing we can do
+            try{
+                if(conn!=null) {
+                    conn.close();
+                }
+            }catch(SQLException se){
+            }//end finally try
+            //end try
+            
+                
+                return data;
+        
+    
+    
+    }
+    
+    public String[][] get_psong(String plname){
+        Connection conn = null;
+        Statement stmt = null;
+        String[][] loc = new String[100][100];
+        //String[] nm = new String[100];
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            //STEP 4: Execute a query
+            //System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "Select Sname,location from OfPlaylist where PlName='"+plname+"'";
+            ResultSet ret1 = stmt.executeQuery(sql);
+            int i=0;
+            while(ret1.next()){
+                loc[i][0] = ret1.getString("location");
+                loc[i][1] = ret1.getString("SName");
+                i = i+1;
+            }
+            loc[i][0] = "eof";
+            loc[i][1] = "eof";
+            
+            
+        }   catch (SQLException ex) {
+                Logger.getLogger(DbSave.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(DbSave.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    
+    try{
+            if(stmt!=null) {
+                stmt.close();
+            }
+            }catch(SQLException se2){
+            }// nothing we can do
+            try{
+                if(conn!=null) {
+                    conn.close();
+                }
+            }catch(SQLException se){
+            }//end finally try
+            //end try
+            return loc;
+    
+    
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
